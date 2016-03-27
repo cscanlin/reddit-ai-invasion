40671c8b7128ab3f6624a7a70873a6b1a589f84e
Chris Scanlin <cscanlin@gmail.com>
2016-03-26 17:11:14 -0700

    working comment adder

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -43,17 +43,16 @@ function lowestST() {
 		};
 };
 
-function STCheck(callback) {
+function STListener(callback) {
     currentST = lowestST();
 		if (probabilityCheck(SubmissionOccurrenceProbability)) {
-			callback(currentST);
+				callback(currentST);
 		};
-		callback(currentST)
-    var STCheck = setInterval(function () {
+    var STListener = setInterval(function () {
       	if (currentST != lowestST()) {
             currentST = lowestST();
 						if (probabilityCheck(SubmissionOccurrenceProbability)) {
-							callback(currentST);
+								callback(currentST);
 						};
         };
     }, 1000);
@@ -67,46 +66,44 @@ function probabilityCheck(probability) {
 	};
 };
 
-function ChangeSubmissionHTML(insertST) {
-		links = insertST.querySelectorAll('div.link:not(.stickied)')
-		randomLink = getRandom(links)
-		newHTML = '<div class=" thing id-t3_4by3vp even  link " id="thing_t3_4by3vp" onclick="click_thing(this)" data-fullname="t3_4by3vp" data-type="link" data-author="all-top-today_SS" data-author-fullname="t2_nycxd" data-subreddit="SubredditSimulator" data-subreddit-fullname="t5_38iwt" data-timestamp="1458935895000" data-url="http://i.imgur.com/2QlYrhI.jpg" data-domain="i.imgur.com" data-rank="8"><p class="parent"></p><span class="rank">8</span><div class="midcol unvoted"><div class="arrow up login-required access-required" data-event-action="upvote" role="button" aria-label="upvote" tabindex="0"></div><div class="score dislikes">35</div><div class="score unvoted">36</div><div class="score likes">37</div><div class="arrow down login-required access-required" data-event-action="downvote" role="button" aria-label="downvote" tabindex="0"></div></div><a class="thumbnail may-blank loggedin " href="http://i.imgur.com/2QlYrhI.jpg"><img src="//a.thumbs.redditmedia.com/n0Ns_FWR_RkIF6nkWEHhity6QdI_3-a26hHiJs_5gZ8.jpg" width="70" height="70" alt=""></a><div class="entry unvoted lcTagged"><p class="title"><a class="title may-blank loggedin  srTagged imgScanned" href="http://i.imgur.com/2QlYrhI.jpg" tabindex="1" type="IMAGE" name="img10">This is the best meme for this leek so they can find them</a> <span class="domain">(<a href="/domain/i.imgur.com/">i.imgur.com</a>)</span></p><a class="toggleImage expando-button collapsed collapsedExpando image linkImg">&nbsp;</a><p class="tagline">submitted <time title="Fri Mar 25 19:58:15 2016 UTC" datetime="2016-03-25T19:58:15+00:00" class="live-timestamp">13 hours ago</time> by <a href="https://www.reddit.com/user/all-top-today_SS" class="author may-blank id-t2_nycxd userTagged">all-top-today_SS</a><span class="RESUserTag"><a class="userTagLink RESUserTagImage" username="all-top-today_ss" title="set a tag" href="javascript:void 0"></a></span> <a href="#" class="voteWeight" style="display: none;">[vw]</a><span class="userattrs"></span></p><ul class="flat-list buttons"><li class="first"><a href="https://www.reddit.com/r/SubredditSimulator/comments/4by3vp/this_is_the_best_meme_for_this_leek_so_they_can/" data-event-action="comments" class="bylink comments may-blank" rel="nofollow">21 comments</a></li><li class="share"><a class="post-sharing-button" href="javascript: void 0;">share</a></li><li class="link-save-button save-button"><a href="#">save</a></li><li><form action="/post/hide" method="post" class="state-button hide-button"><input type="hidden" name="executed" value="hidden"><span><a action="hide" href="#">hide</a></span></form></li><li class="report-button"><a href="javascript:void(0)" class="reportbtn access-required" data-event-action="report">report</a></li><li><span class="redditSingleClick" thislink="http://i.imgur.com/2QlYrhI.jpg" thiscomments="https://www.reddit.com/r/SubredditSimulator/comments/4by3vp/this_is_the_best_meme_for_this_leek_so_they_can/">[l+c]</span></li></ul><div class="reportform report-t3_4by3vp"></div><div class="expando expando-uninitialized" style="display: none"><span class="error">loading...</span></div></div><div class="child"></div><div class="clearleft"></div></div>'
-		$(randomLink).replaceWith(newHTML);
-}
-
-function AddLink() {
-		var botUsername = getBotUser(fuzzinessLimit)
-		STCheck(function (callbackArg) {
-	  		ChangeSubmissionHTML(callbackArg);
-		});
-}
-
-function getCommentData(username) {
-		var top100Comments = null;
+function getUserContentData(username, contentType) {
+		var top100 = null;
 		$.ajax({
-			 url: "https://api.reddit.com/user/" + username + "/comments",
+			 url: "https://api.reddit.com/user/" + username + "/" + contentType,
 			 data: {'sort': 'top', 'limit': 100},
 			 type: 'get',
 			 dataType: 'json',
 			 async: false,
 			 success: function(response) {
-					 top100Comments = response.data.children;
+					 top100 = response.data.children;
 			 }
 		});
-		return parseCommentData(top100Comments);
+		var contentData = getRandom(top100).data
+
+		if (contentType == 'comments') {
+				return parseCommentData(contentData);
+		} else if (contentType == 'submitted') {
+				return parseSubmissionData(contentData);
+		};
 }
 
-function parseCommentData(top100Comments) {
-		var commentData = getRandom(top100Comments).data;
+function parseCommentData(commentData) {
 		var parsed_link_id = commentData.link_id.split('_')[1];
 		commentData.link_id = parsed_link_id;
 		commentData.body_html = $('<textarea />').html(commentData.body_html).text();
 		return commentData;
 }
 
-function AddComment(commentTemplate) {
+function parseSubmissionData(submissionData) {
+		// var parsed_link_id = commentData.link_id.split('_')[1];
+		// commentData.link_id = parsed_link_id;
+		// commentData.body_html = $('<textarea />').html(commentData.body_html).text();
+		return submissionData;
+}
+
+function addComment(commentTemplate) {
 		var botUsername = getBotUser(fuzzinessLimit);
-		var botCommentData = getCommentData(botUsername);
+		var botCommentData = getUserContentData(botUsername, 'comments');
 
 		var renderedComment = Mustache.render(commentTemplate, botCommentData);
 		var parentComments = $('.nestedlisting').children('.comment');
@@ -114,10 +111,23 @@ function AddComment(commentTemplate) {
 		randomComment.insertAdjacentHTML('afterend', renderedComment);
 }
 
+function addLink(submissionTemplate, insertST) {
+		var botUsername = getBotUser(fuzzinessLimit)
+		var botSubmissionData = getUserContentData(botUsername, 'submitted')
+		console.log(botSubmissionData);
+
+		var renderedLink = Mustache.render(submissionTemplate, botSubmissionData);
+		var links = insertST.querySelectorAll('div.link:not(.stickied)')
+		var randomLink = getRandom(links)
+		randomLink.insertAdjacentHTML('afterend', renderedLink);
+}
+
 if (isComments()) {
-	if (probabilityCheck(CommentOccurrenceProbability)) {
-		AddComment(commentTemplate)
-	}
-} else {
-		AddLink()
+		if (probabilityCheck(CommentOccurrenceProbability)) {
+				addComment(commentTemplate)
+		}
+	} else {
+		STListener(function (callbackArg) {
+				addLink(submissionTemplate, callbackArg);
+		});
 }
